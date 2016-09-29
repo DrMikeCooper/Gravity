bb6af9058c627c989365a6bf684f87ef2fb880f4
Dr_Mike_Cooper <coriolisboy@gmail.com>
2016-09-29 12:45:01 +0930

    Initial Checkin

---
+++ Assets/GravityPlane.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityPlane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        GravityPlayer player = col.GetComponent<GravityPlayer>();
+        if (player != null && player.current != this)
+        {
+            //col.transform.up = transform.up;
+            Vector3 right = Vector3.Cross(col.transform.up, transform.up);
+
+            col.transform.rotation = Quaternion.AngleAxis(90, right) * col.transform.rotation;
+
+            player.current = this;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        GravityPlayer player = col.GetComponent<GravityPlayer>();
+        if (player != null && player.current == this)
+        {
+            player.current = null;
+        }
+    }
+}
---
+++ Assets/GravityPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityPlayer : MonoBehaviour {
+
+    public GravityPlane current;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position
+            + 0.1f * transform.right * Input.GetAxis("Horizontal")
+            + 0.1f * transform.forward * Input.GetAxis("Vertical");
+
+        transform.rotation = Quaternion.AngleAxis(Input.GetAxis("Mouse X"), transform.up) * transform.rotation;
+	}
+}
